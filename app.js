f0b1c17973d9c08b912aaef61b464c5f824cb198
Stefan Koev <st.koev@outlook.com>
2022-08-29 19:46:21 +0300

    display items with js

--- app.js
+++ app.js
@@ -72,3 +72,29 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+const sectionCenter = document.querySelector(".section-center");
+
+window.addEventListener("DOMContentLoaded", function () {
+  const items = displayMenuItems(menu);
+  sectionCenter.insertAdjacentHTML("afterbegin", items);
+});
+
+const displayMenuItems = function (menuItems) {
+  const displayMenu = menuItems.map((item) => {
+    return `
+    <article class="menu-item">
+          <img src="${item.img}" class="photo" alt="${item.title}" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">${item.desc}
+            </p>
+          </div>
+        </article>
+    `;
+  });
+  return displayMenu.join("");
+};
